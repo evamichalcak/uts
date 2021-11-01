6e6d92937a1f89d421bdc4027bf495d09560f337
Eva Michalcak <evamichalcak@users.noreply.github.com>
2021-11-01 14:19:28 +0100

    add ids in todoArray

--- data/dataTodos.js
+++ data/dataTodos.js
@@ -1,13 +1,16 @@
 todoArray = [
     {
+        id: 1,
         text: "Clean up",
         done: true,
     },
     {
+        id: 2,
         text: "Put the kids to bed",
         done: false,
     },
     {
+        id: 3,
         text: "Study Javascript",
         done: false,
     },
