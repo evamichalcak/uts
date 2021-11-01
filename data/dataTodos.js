c99336e8bf00c1e0247e754223cdc3f4b83e8c1c
Eva Michalcak <evamichalcak@users.noreply.github.com>
2021-11-01 13:49:25 +0100

    initial files

---
+++ data/dataTodos.js
@@ -0,0 +1,14 @@
+todoArray = [
+    {
+        text: "Clean up",
+        done: true,
+    },
+    {
+        text: "Put the kids to bed",
+        done: false,
+    },
+    {
+        text: "Study Javascript",
+        done: false,
+    },
+]
