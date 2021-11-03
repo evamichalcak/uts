c3fa443b3721c5698621df9d8931dff943317f8d
Eva Michalcak <evamichalcak@users.noreply.github.com>
2021-11-03 19:25:38 +0100

    new exercise contacts

---
+++ contacts/data/dataContacts.js
@@ -0,0 +1,17 @@
+todoArray = [
+    {
+        id: 1,
+        name: "Eva",
+        number: "679107784",
+    },
+    {
+        id: 1,
+        name: "Dan",
+        number: "665327981",
+    },
+    {
+        id: 3,
+        name: "Carole",
+        number: "615361326",
+    },
+]
---
+++ todo/js/templates/todoList.js
@@ -0,0 +1,12 @@
+<ul>
+    <li id="todo-1" class="done">
+        Cook food
+        <input type="checkbox" id="done-1" checked="true">
+        <button id="delete-1">X</button>
+    </li>
+    <li id="todo-2" class="">
+        Eat food
+        <input type="checkbox" id="done-2" checked="false">
+        <button id="delete-2">X</button>
+    </li>
+</ul>
